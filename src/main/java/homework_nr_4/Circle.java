438ac65d83e81ca26b4cd1f83ab59dc9990af55d
Lenovo <caglarboiraz@gmail.com>
2025-01-09 00:29:56 +0200

    fixed name calculateRadius as calculateArea added constructor Circle added object Circle1

--- src/main/java/homework_nr_4/Circle.java
+++ src/main/java/homework_nr_4/Circle.java
@@ -1,15 +1,27 @@
 package homework_nr_4;
 
 public class Circle {
+
+    static int radius;
+
+    public Circle (int radius) {
+        this.radius = radius;
+    }
+
     public static void main (String[] args){
 
-        int radius = 3;
-        double r = calculateRadius(radius);
-        System.out.println(r);
-    }
-    public static float calculateRadius (int inputNumber){
+        Circle Circle1 = new Circle(4);
 
-        float result = (float) ((inputNumber * inputNumber) * 3.14f);
+        float area = Circle1.calculateArea();
+
+        System.out.println(area);
+
+    }
+
+    public static float calculateArea (){
+
+        float result = (float) ((radius * radius) * 3.14f);
         return result;
     }
+
 }
